eb61f8e149bd272f1404b7aa7487a71d3237c91f
Silvana Mora <silvanamorachaves@gmail.com>
2023-10-30 18:12:23 -0500

    centrado de home y login

--- src/home.js
+++ src/home.js
@@ -8,19 +8,35 @@
 
 function home(navigateTo) {
   const section = document.createElement('section');
+  section.setAttribute('id', 'sectionHome') //agregamos
+
+  //Logo 
+  const img = document.createElement('img');
+  img.src = 'https://github.com/carolcarim/DEV011-Social-Network-SN13/blob/main/src/Images/Drink%20Atlas%20Logo.png?raw=true';
+  img.alt = 'Logo de Drink Atlas';
+  img.id = 'Drink';
+  document.body.appendChild(img); // agregamos el documeto de imagen al documento body
+
+  //Titulo
   const title = document.createElement('h2');
-  const button = document.createElement('button');
- //se AGREG
-button.addEventListener("click", () =>
+  title.textContent = 'Bienvenido a Drink Atlas';
+
+  //Boton iniciar sesion
+  const buttonIniciarSesion = document.createElement('button'); //creamos el elemento boton
+  buttonIniciarSesion.textContent = 'Iniciar sesión'; //contenido del boton
+  buttonIniciarSesion.addEventListener("click", () => //evento
 {
 navigateTo('/login');
 
 });
 
-  button.textContent = 'hola';
-  title.textContent = 'Bienvenido a Drink Atlas';
+// Boton registrarse
+  const buttonRegistrarse = document.createElement('button');
+  buttonRegistrarse.textContent = 'Registrarse';
 
-  section.append(title, button);
+  
+//Para agregar los elementos a la seccion
+  section.append(title, buttonIniciarSesion, buttonRegistrarse,);
   return section;
 }
 export default home;
--- src/login.js
+++ src/login.js
@@ -1,21 +1,34 @@
 // file login.js
 
-function login() {
+function login(navigateTo) {
   const section = document.createElement('section');
+  section.setAttribute('id', 'sectionLogin') //agregamos
+
   const title = document.createElement('h2');
-  const buttonReturn = document.createElement('button');//Falta crear evento
+
+  //funcion boton regresar
+  const buttonReturn = document.createElement('button'); //creamos el  boton
+  buttonReturn.textContent = 'regresar'; //agregamos nombre al boton
+  buttonReturn.classList.add('btnReturn'); //añadimos clase 
+  buttonReturn.addEventListener('click', () => { //evento 
+  navigateTo('/')
+  });
+
   const form = document.createElement('form');
   const inputEmail = document.createElement('input');
   const inputPass = document.createElement('input');
+  //funcion boton login
   const buttonLogin = document.createElement('button');//falta crear evento
+
   inputEmail.placeholder = 'Ingresa tu correo';
   inputPass.placeholder = 'contraseña';
   title.textContent = 'login';
   buttonLogin.textContent = 'ir';
-  buttonReturn.textContent = 'regresar';
+  
 
   form.append(inputEmail, inputPass, buttonLogin);
   section.append(title, form, buttonReturn);
   return section;
 }
 export default login;
+
